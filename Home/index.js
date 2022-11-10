2c8b288216159b82a86ea30ba6510819c57af39e
mdfaizan973 <faizan.md9735@gmail.com>
2022-11-11 01:41:21 +0530

    Added search functionality to landing page

---
+++ Home/index.js
@@ -0,0 +1,382 @@
+
+console.log("faizan")
+let arr =[
+    {
+        'img':'https://static.thcdn.com/images/xsmall/original//productimg/original/12673805-1194865842860753.jpg',
+        'pro_name':'AMELIORATE Transforming Scalp Serum 125ml',
+        'rating':'⭐⭐⭐⭐',
+        'price':"27.00"
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp/productimg/1600/1600/11482652-3864609764268347.jpg',
+        'pro_name':'Philip Kingsley Elasticizer Extreme 5 oz.',
+        'rating':'⭐⭐⭐⭐',
+        'price':"55.00"
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11352854-1104873645631466.jpg',
+        'pro_name':'Philip Kingsley Elasticizer Therapies 2021 Egyptian Jasmine & Mandarin 150ml',
+        'rating':'⭐⭐⭐⭐',
+        'price':"88.00"
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11517733-1094873815535035.jpg',
+        'pro_name':'Philip Kingsley Elasticizer 2.5 oz.',
+        'rating':'⭐⭐⭐⭐',
+        'price':"33.00"
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11401780-1544963535783625.jpg',
+        'pro_name':'Alterna Caviar Anti-Aging Smoothing Anti-Frizz Nourishing Oil',
+        'rating':'⭐⭐⭐⭐',
+        'price':"20.00"
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/12362849-1204945676856453.jpg',
+        'pro_name':'Philip Kingsley Elasticizer Therapies 2021 Egyptian Jasmine & Mandarin 150ml',
+        'rating':'⭐⭐⭐⭐',
+        'price':"30.00"
+    }, {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/12538164-2124967415232770.jpg',
+        'pro_name':'Philip Kingsley Elasticizer Extreme Rich Deep-Conditioning Treatment 500ml (Worth $163)',
+        'rating':'⭐⭐⭐⭐',
+        'price':"99.00"
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/1600/1600/10780664-1024713431790807.jpg',
+        'pro_name':'Philip Kingsley Elasticizer 2.5 oz.',
+        'rating':'⭐⭐⭐⭐',
+        'price':"20.00"
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/1600/1600/10780664-1024713431790807.jpg',
+        'pro_name':'Philip Kingsley Elasticizer (1000ml, Worth $327)',
+        'rating':'⭐⭐⭐⭐',
+        'price':"30.00"
+    }, {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/1600/1600/12538162-4054763149768374.jpg',
+        'pro_name':'Philip B Everyday Beautiful Conditioner 6 fl. oz',
+        'rating':'⭐⭐⭐⭐',
+        'price':"99.00"
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11401780-1544963535783625.jpg',
+        'pro_name':'Alterna Caviar Anti-Aging Smoothing Anti-Frizz Nourishing Oil',
+        'rating':'⭐⭐⭐⭐',
+        'price':"20.00"
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/12362849-1204945676856453.jpg',
+        'pro_name':'Philip Kingsley Elasticizer Therapies 2021 Egyptian Jasmine & Mandarin 150ml',
+        'rating':'⭐⭐⭐⭐',
+        'price':"30.00"
+    }, {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/12538164-2124967415232770.jpg',
+        'pro_name':'Philip Kingsley Elasticizer Extreme Rich Deep-Conditioning Treatment 500ml (Worth $163)',
+        'rating':'⭐⭐⭐⭐',
+        'price':"99.00"
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11352854-1104873645631466.jpg',
+        'pro_name':'Philip Kingsley Elasticizer Therapies 2021 Egyptian Jasmine & Mandarin 150ml',
+        'rating':'⭐⭐⭐⭐',
+        'price':"88.00"
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11517733-1094873815535035.jpg',
+        'pro_name':'Philip Kingsley Elasticizer 2.5 oz.',
+        'rating':'⭐⭐⭐⭐',
+        'price':"33.00"
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11401780-1544963535783625.jpg',
+        'pro_name':'Alterna Caviar Anti-Aging Smoothing Anti-Frizz Nourishing Oil',
+        'rating':'⭐⭐⭐⭐',
+        'price':"20.00"
+    },
+    
+
+        {
+        "img":"https://static.thcdn.com/images/xsmall/webp//productimg/original/11287458-9774871766506958.jpg",
+        'pro_name':'Epionce Renewal Facial Cream',
+        'rating':'⭐⭐⭐⭐',
+        'price':'81.68',
+        },
+        {
+            "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11287444-1174871762980791.jpg",
+            'pro_name':'Epionce Intensive Nourishing Cream',
+            'rating':'⭐⭐⭐⭐⭐',
+            'price':'96.00',
+        },
+        {
+            "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11207544-1114918471847064.jpg",
+        'pro_name':'DHC Lip Cream',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'90.00',
+        },
+        {
+            "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13457816-4434917119899269.jpg",
+        'pro_name':'C.O. Bigelow Premium Shave Cream 5.2ml',
+        'rating':'⭐⭐⭐⭐',
+        'price':'10.00',
+        },
+        {
+            "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11054488-6294867924186974.jpg",
+        'pro_name':'First Aid Beauty Ultra Repair Cream',
+        'rating':'⭐⭐⭐',
+        'price':'78.00',
+        },
+        {
+            "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12903093-1164871741627199.jpg",
+        'pro_name':'Neocutis BIO Cream Firm 1.69 fl. oz.',
+        'rating':'⭐⭐⭐⭐',
+        'price':'178.00',
+        },
+        {
+            "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11287457-3854871985878184.jpg",
+        'pro_name':'Epionce Renewal Eye Cream',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'67.00',
+        },
+        {
+            "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12664554-1004871739941589.jpg",
+        'pro_name':'Neocutis Lumière Firm Illuminating and Tightening Eye Cream 15ml',
+        'rating':'⭐⭐⭐⭐',
+        'price':'108.00',
+        },
+        {
+            "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11287572-1074867924814868.jpg",
+        'pro_name':'First Aid Beauty Ultra Repair Cream',
+        'rating':'⭐⭐⭐⭐',
+        'price':'16.00',
+        },
+        {
+        "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11401173-1104945375256001.jpg",
+        'pro_name':'COSRX Advanced Snail 92 All in One Cream 100ml',
+        'rating':'⭐⭐⭐',
+        'price':'26.00',
+        },
+        {
+            "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12903093-1164871741627199.jpg",
+        'pro_name':'Neocutis BIO Cream Firm 1.69 fl. oz.',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'178.00',
+        },
+    {
+        "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11903200-9774871739506130.jpg",
+        'pro_name':'Neocutis NEO FIRM Micro Firm Neck & Décolleté Rejuvenating Complex and Tightening Cream',
+        'rating':'⭐⭐',
+        'price':'135.00',
+    },
+    {
+        "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11380203-8304871739045636.jpg",
+        'pro_name':'Neocutis Journée SPF 30 Bio-Restorative Revitalizing Day Cream with PSP',
+        'rating':'⭐⭐⭐',
+        'price':'189.00',
+    },
+    {
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11401780-1544963535783625.jpg',
+        'pro_name':'Alterna Caviar Anti-Aging Smoothing Anti-Frizz Nourishing Oil',
+        'rating':'⭐⭐⭐⭐',
+        'price':"20.00"
+    },
+    {
+        "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13643076-1644934556718788.jpg",
+        'pro_name':'Bioderma Duo Atoderm Cream 500ml',
+        'rating':'⭐⭐⭐⭐',
+        'price':'31.99',
+    },
+    {
+        "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12378444-2084870995827076.jpg",
+        'pro_name':'SkinMedica Instant Bright Eye Cream 5 oz',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'82.00',
+    },
+    {
+        "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289681-1484871019698619.jpg",
+        'pro_name':'SkinMedica TNS Ceramide Treatment Cream',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'64.00',
+    },
+    {
+        "img":"https://static.thcdn.com/images/xsmall/webp//productimg/original/12932548-2534984326440526.jpg",
+        'pro_name':'SkinCeuticals Phyto Corrective Essence Mist 50ml',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'67.00',
+        },
+        {
+            "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13036144-1644892769995985.jpg",
+            'pro_name':'SkinCeuticals Daily Brightening UV Defense Sunscreen SPF30 1 fl. oz',
+            'rating':'⭐⭐⭐⭐⭐',
+            'price':'56.00',
+        },
+        
+        
+        
+        {
+            "img":"https://static.thcdn.com/images/xsmall/webp//productimg/original/13516546-1514932677894190.jpg",
+            'pro_name':'SkinCeuticals Phyto A+ Brightening Treatment 1 fl. oz',
+            'rating':'⭐⭐⭐⭐⭐',
+            'price':'105.00',
+            },
+        
+        
+        
+        {
+            "img":"https://static.thcdn.com/images/xsmall/webp//productimg/original/13279308-1414898791289589.jpg",
+            'pro_name':'SkinCeuticals Post-Chemical Peel System',
+            'rating':'⭐⭐⭐⭐⭐',
+            'price':'200.00',
+            },
+        
+        
+                
+        {
+        "img":"https://static.thcdn.com/images/xsmall/webp//productimg/original/13279307-1504898790753222.jpg",
+        'pro_name':'SkinCeuticals Post-Injectable System',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'190.00',
+        },
+
+        {
+        "img":"https://static.thcdn.com/images/xsmall/webp//productimg/original/13196078-1324889940505536.jpg",
+        'pro_name':'SkinCeuticals Advanced Scar Control 1.7 fl. oz',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'190',
+        },
+
+        {
+        "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13036144-1644892769995985.jpg",
+        'pro_name':'SkinCeuticals Daily Brightening UV Defense Sunscreen SPF30 1 fl. oz',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'56.00',
+        },
+
+
+
+        {
+        "img":"https://static.thcdn.com/images/xsmall/webp//productimg/original/13156809-1574915281422679.jpg",
+        'pro_name':'TriPollar STOP Vx',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'649.00',
+        },{
+        "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13447267-2014911154901895.jpg",
+        'pro_name':'TriPollar Stop Vx - Gold Edition',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'849.00',
+        },{
+        "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12730668-2034926462636648.jpg",
+        'pro_name':'TriPollar GENEO PERSONAL 4 Treatment Kit',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'39.00',
+        },
+        {
+            "img":"https://static.thcdn.com/images/xsmall/webp//productimg/original/13516546-1514932677894190.jpg",
+            'pro_name':'SkinCeuticals Phyto A+ Brightening Treatment 1 fl. oz',
+            'rating':'⭐⭐⭐⭐⭐',
+            'price':'105.00',
+            },
+
+
+
+{
+    "img":"https://static.thcdn.com/images/xsmall/webp//productimg/original/13279308-1414898791289589.jpg",
+    'pro_name':'SkinCeuticals Post-Chemical Peel System',
+    'rating':'⭐⭐⭐⭐⭐',
+    'price':'200.00',
+    },
+    {
+    "img":"https://static.thcdn.com/images/xsmall/webp//productimg/original/12785323-9154915281250482.jpg",
+    'pro_name':'TriPollar STOP X ROSE Ombre',
+    'rating':'⭐⭐⭐⭐⭐',
+    'price':'224.50',
+    },{
+        "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12358556-1974888087906815.jpg",
+        'pro_name':'TriPollar STOP X Device - Rose Gold',
+        'rating':'⭐⭐⭐⭐⭐',
+        'price':'199.75',
+        },{
+            "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13432965-1024903973866227.jpg",
+            'pro_name':'TriPollar x U Beauty Skin Care Bundle',
+            'rating':'⭐⭐⭐⭐⭐',
+            'price':'655.00',
+
+
+        },
+
+        {
+            "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13694395-1094938373792395.jpg",
+            'pro_name':'TriPollar STOP Vx and Cosmetics Bag Exclusive Bundle',
+            'rating':'⭐⭐⭐⭐⭐',
+            'price':'674.00',
+            },
+
+
+]
+
+console.log(arr)
+
+function search(){
+    let data =document.querySelector('#search').value
+    console.log(data)
+
+
+    let new_data=arr.filter(function (el){
+
+        return el.pro_name.toLowerCase().includes(data.toLowerCase())
+
+    })
+display(new_data)
+}
+
+
+const  display=(data )=>{
+
+    document.querySelector('#homeui').innerHTML=''
+
+    
+
+    data.forEach((el) => {
+        // console.log(el)
+
+        // document.querySelector('#pname').innerHTML=value
+        
+        let card=document.createElement('div')
+        card.setAttribute('class', 'card')
+
+        let im= document.createElement('img')
+        im.src=el.img
+
+        let nm=document.createElement('h4')
+        nm.innerHTML=el.pro_name
+
+        let off_btn=document.createElement('h5')
+        off_btn.innerHTML=' 25% off with code SINGLES'
+
+        let rat=document.createElement('p')
+        rat.innerHTML=el.rating
+
+        let price=document.createElement('h2')
+        price.innerHTML='$' + el.price
+
+        let sh_btn=document.createElement('button')
+        sh_btn.innerHTML='QUICK BUY'
+
+        sh_btn.addEventListener('click', () =>{
+
+            buydata.push(el)
+            localStorage.setItem('cart',JSON.stringify(buydata))
+
+        })
+
+
+
+        card.append(im,nm,off_btn,rat,price,sh_btn)
+
+        document.querySelector('#proui').append(card)
+
+    })
+
+}
+
+let buydata = JSON.parse(localStorage.getItem('cart')) || []
