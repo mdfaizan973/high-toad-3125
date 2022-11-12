280cc894f201996137b96e50cb412ea6705e3601
mdfaizan973 <faizan.md9735@gmail.com>
2022-11-13 01:30:53 +0530

    Added filter and sorting

--- product/prodin.js
+++ product/prodin.js
@@ -5,7 +5,8 @@
         'pro_name':'Philip Kingsley Elasticizer  500ml (Worth $163)',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"99.00"
+        'price':"99.00",
+        'catagory':"cream"
     },
   
     {
@@ -13,81 +14,160 @@
         'pro_name':'AMELIORATE Transforming Scalp Serum 125ml',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"27.00"
+        'price':"27.00",
+        'catagory':"serum"
     },
+    {
+        
+        'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/13046276-1234938920413647.jpg',
+        'pro_name':'GLO Science Teeth Whitening Glo Vials - Cinnamint (3 Pack)',
+        'rating':'⭐⭐⭐⭐',
+        'price':"24.00",
+        'catagory':"cream"
+    
+        },
+        
+        {
+            'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/13440251-1034944429858543.jpg',
+            'pro_name':'GLO Science Teeth Whitening GLO® Vials 5 Pack - Cinnamint Flavor',
+            'rating':'⭐⭐⭐⭐',
+            'price':"25.00",
+            'catagory':"oil"
+        },
+    
+        {
+            'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/12711543-1984907286241809.jpg',
+            'pro_name':'GLO Science GLO Lit Teeth Whitening Device Tech Kit with Bluetooth',
+            'rating':'⭐⭐⭐⭐',
+            'price':"26.00",
+            'catagory':"oil"
+        },
+    
+        {
+            'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/13645157-1654947241826904.jpg',
+            'pro_name':'RAINCRY Regenerating Shampoo and Repairing Conditioner Bundle',
+            'rating':'⭐⭐⭐⭐',
+            'price':"27.00",
+        'catagory':"oil"
+
+        },
+    
+        {
+            'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/12507322-1384942067118420.jpg',
+            'pro_name':'RAINCRY Daily Densifying Treatment 5.1oz',
+            'rating':'⭐⭐⭐⭐',
+        'catagory':"serum",
+        'price':"28.00",
+        },
+    
+        {
+            'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/13908249-9164977248545545.jpg',
+            'pro_name':'RAINCRY Volume and Body Bundle',
+            'rating':'⭐⭐⭐⭐',
+        'catagory':"serum",
+        'price':"29.00",
+        },
+    
+        {
+            'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/12507333-2154942066999833.jpg',
+            'pro_name':'RAINCRY Detangle Travel Detangling Paddle Brush',
+            'rating':'⭐⭐⭐⭐',
+            'price':"30.00",
+            'catagory':"oil"
+        },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/1600/1600/10780664-1024713431790807.jpg',
         'pro_name':'Philip Kingsley Elasticizer (1000ml, Worth $327)',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"30.00"
+        'price':"30.00",
+        'catagory':"serum"
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/1600/1600/10780664-1024713431790807.jpg',
         'pro_name':'Philip Kingsley Elasticizer  230ml 2.5 oz.',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"20.00"
+        'price':"20.00",
+        'catagory':"oil"
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp/productimg/1600/1600/11289602-1904469032356138.jpg',
         'pro_name':'AMELIORATE Transforming Scalp Serum 125ml',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"27.00"
+        'price':"27.00",
+        'catagory':"serum"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp/productimg/1600/1600/11482652-3864609764268347.jpg',
         'pro_name':'Philip Kingsley Elasticizer Extreme 5 oz.',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"55.00"
+        'price':"55.00",
+        'catagory':"oil"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11352854-1104873645631466.jpg',
         'pro_name':'Philip Kingsley Elasticizer Therapies 2021 Egyptian Jasmine & Mandarin 150ml',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"88.00"
+        'price':"88.00",
+        'catagory':"serum"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11517733-1094873815535035.jpg',
         'pro_name':'Philip Kingsley Elasticizer 2.5 oz.',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"33.00"
+        'price':"33.00",
+        'catagory':"cream"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11401780-1544963535783625.jpg',
         'pro_name':'Alterna Caviar Anti-Aging Smoothing Anti-Frizz Nourishing Oil',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"20.00"
+        'price':"20.00",
+        'catagory':"cream"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/12362849-1204945676856453.jpg',
         'pro_name':'Philip Kingsley Elasticizer Therapies 2021 Egyptian Jasmine & Mandarin 150ml',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"30.00"
+        'price':"30.00",
+        'catagory':"cream"
+
     }, {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/12538164-2124967415232770.jpg',
         'pro_name':'Philip Kingsley Elasticizer Extreme Rich Deep-Conditioning Treatment 500ml (Worth $163)',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"99.00"
+        'price':"99.00",
+        'catagory':"oil"
+
     },
     {
         "img":"https://static.thcdn.com/images/xsmall/webp//productimg/original/12932548-2534984326440526.jpg",
         'pro_name':'SkinCeuticals Phyto Corrective Essence Mist 50ml',
         'rating':'⭐⭐⭐⭐⭐',
         'price':'67.00',
+        'catagory':"cream"
+
         },
         {
             "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13036144-1644892769995985.jpg",
             'pro_name':'SkinCeuticals Daily Brightening UV Defense Sunscreen SPF30 1 fl. oz',
             'rating':'⭐⭐⭐⭐⭐',
             'price':'56.00',
+        'catagory':"oil"
+
         },
         
         
@@ -97,6 +177,8 @@
             'pro_name':'SkinCeuticals Phyto A+ Brightening Treatment 1 fl. oz',
             'rating':'⭐⭐⭐⭐⭐',
             'price':'105.00',
+        'catagory':"serum"
+
             },
         
         
@@ -106,6 +188,8 @@
             'pro_name':'SkinCeuticals Post-Chemical Peel System',
             'rating':'⭐⭐⭐⭐⭐',
             'price':'200.00',
+        'catagory':"cream"
+
             },
         
         
@@ -115,6 +199,8 @@
         'pro_name':'SkinCeuticals Post-Injectable System',
         'rating':'⭐⭐⭐⭐⭐',
         'price':'190.00',
+        'catagory':"oil"
+
         },
 
         {
@@ -122,6 +208,8 @@
         'pro_name':'SkinCeuticals Advanced Scar Control 1.7 fl. oz',
         'rating':'⭐⭐⭐⭐⭐',
         'price':'190',
+        'catagory':"serum"
+
         },
 
         {
@@ -129,6 +217,8 @@
         'pro_name':'SkinCeuticals Daily Brightening UV Defense Sunscreen SPF30 1 fl. oz',
         'rating':'⭐⭐⭐⭐⭐',
         'price':'56.00',
+        'catagory':"oil"
+
         },
 
 
@@ -138,22 +228,30 @@
         'pro_name':'TriPollar STOP Vx',
         'rating':'⭐⭐⭐⭐⭐',
         'price':'649.00',
+        'catagory':"cream"
+
         },{
         "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13447267-2014911154901895.jpg",
         'pro_name':'TriPollar Stop Vx - Gold Edition',
         'rating':'⭐⭐⭐⭐⭐',
         'price':'849.00',
+        'catagory':"cream"
+
         },{
         "img":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12730668-2034926462636648.jpg",
         'pro_name':'TriPollar GENEO PERSONAL 4 Treatment Kit',
         'rating':'⭐⭐⭐⭐⭐',
         'price':'39.00',
+        'catagory':"cream"
+
         },
         {
             "img":"https://static.thcdn.com/images/xsmall/webp//productimg/original/13516546-1514932677894190.jpg",
             'pro_name':'SkinCeuticals Phyto A+ Brightening Treatment 1 fl. oz',
             'rating':'⭐⭐⭐⭐⭐',
             'price':'105.00',
+        'catagory':"cream"
+
             },
 
 
@@ -162,68 +260,88 @@
         'pro_name':'Philip Kingsley Elasticizer 2.5 oz.',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"20.00"
+        'price':"20.00",
+        'catagory':"oil"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/1600/1600/10780664-1024713431790807.jpg',
         'pro_name':'Philip Kingsley Elasticizer (1000ml, Worth $327)',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"30.00"
+        'price':"30.00",
+        'catagory':"oil"
+
     }, {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/1600/1600/12538162-4054763149768374.jpg',
         'pro_name':'Philip B Everyday Beautiful Conditioner 6 fl. oz',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"99.00"
+        'price':"99.00",
+        'catagory':"oil"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11517733-1094873815535035.jpg',
         'pro_name':'Philip Kingsley Elasticizer 2.5 oz.',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"33.00"
+        'price':"33.00",
+        'catagory':"oil"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11401780-1544963535783625.jpg',
         'pro_name':'Alterna Caviar Anti-Aging Smoothing Anti-Frizz Nourishing Oil',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"20.00"
+        'price':"20.00",
+        'catagory':"oil"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/12362849-1204945676856453.jpg',
         'pro_name':'Philip Kingsley Elasticizer Therapies 2021 Egyptian Jasmine & Mandarin 150ml',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"30.00"
+        'price':"30.00",
+        'catagory':"serum"
+
     }, {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/12538164-2124967415232770.jpg',
         'pro_name':'Philip Kingsley Elasticizer Extreme Rich Deep-Conditioning Treatment 500ml (Worth $163)',
         'rating':'⭐⭐⭐⭐',
         // "btn":"Select Your Gift",
-        'price':"99.00"
+        'price':"99.00",
+        'catagory':"serum"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11352854-1104873645631466.jpg',
         'pro_name':'Philip Kingsley Elasticizer Therapies 2021 Egyptian Jasmine & Mandarin 150ml',
         'rating':'⭐⭐⭐⭐',
         // "btn":"Select Your Gift",
-        'price':"88.00"
+        'price':"88.00",
+        'catagory':"serum"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11517733-1094873815535035.jpg',
         'pro_name':'Philip Kingsley Elasticizer 2.5 oz.',
         'rating':'⭐⭐⭐⭐',
         // "btn":"Select Your Gift",
-        'price':"33.00"
+        'price':"33.00",
+        'catagory':"serum"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11289638-5054870995605268.jpg',
         'pro_name':'SkinCeuticals Retinol 1.0',
         'rating':'⭐⭐⭐⭐',
         // "btn":"Select Your Gift",
-        'price':"92.00"
+        'price':"92.00",
+        'catagory':"cream"
+
     },
 
     {
@@ -231,32 +349,41 @@
         'pro_name':'SkinCeuticals A.G.E. Interrupter',
         'rating':'⭐⭐⭐⭐',
         // "btn":"Select Your Gift",
-        'price':"168.00"
+        'price':"168.00",
+        'catagory':"cream"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11289609-1174892770940184.jpg',
         'pro_name':'SkinCeuticals Anti-aging Skin Care Routine (Worth $481.00)',
         'rating':'⭐⭐⭐⭐',
         // "btn":"Select Your Gift",
-        'price':"169.00"
+        'price':"169.00",
+        'catagory':"cream"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11517733-1094873815535035.jpg',
         'pro_name':'Philip Kingsley Elasticizer 2.5 oz.',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"123.00"
+        'price':"123.00",
+        'catagory':"cream"
+
     },
     {
         'img':'https://static.thcdn.com/images/xsmall/webp//productimg/original/11292113-6224892768320758.jpg',
         'pro_name':'SkinCeuticals Phloretin CF Gel',
                 'rating':'⭐⭐⭐⭐',
 
-        'price':"72.00"
+        'price':"72.00",
+        'catagory':"oil"
+
     },
 ]
 
 
+console.log(arr)
 // console.log(arr)
 let data= document.querySelector('#main_pro')
 
@@ -275,14 +402,32 @@ arr.sort((a,b)=> a.price-b.price)
 arr.sort((a,b)=> b.price-a.price)
   }
 
-  console.log(arr)
+//   console.log(arr)
   display(arr)
+
 }
+
+
+
+
+
+document.querySelector('#filnm').addEventListener('change', function(){
+    let data =document.querySelector('#filnm').value
+    console.log(data)
+
+
+    let new_data=arr.filter(function (el){
+
+        return el.catagory===data
+
+    })
+display(new_data)
+})
 
 
 document.querySelector('#sel').addEventListener('change', function (){
     let filt = document.querySelector('#sel').value
-    console.log(filt)
+    // console.log(filt)
 
     if(filt=='ltoh'){
       arr.sort((a,b) => a.price - b.price)
@@ -348,5 +493,6 @@ function display(arr){
     })
 
 }
+
 
 display(arr)
